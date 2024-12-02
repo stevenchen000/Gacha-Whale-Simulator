f3d53353c21cb583f6ab107ac71fbdf5e1eaa76b
stevenchen000 <10522779+stevenchen000@users.noreply.github.com>
2024-12-01 17:43:52 -0800

    Dialogue (Small Changes)
    Added a left and right character in the dialogue system

--- Scripts/Dialogue System/DialogueManager.cs
+++ Scripts/Dialogue System/DialogueManager.cs
@@ -4,17 +4,23 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace DialogueSystem
 {
-	public partial class DialogueManager : Node
+	public partial class DialogueManager : CanvasLayer
 	{
 		private static DialogueManager instance;
 
-		[Export] private Node dialogueBox;
+		[Export] private Control dialogueBox;
 		[Export] private Label nameLabel;
 		[Export] private Label textLabel;
-        [Export] private int charactersPerSecond = 30;
+		[Export] private TextureRect leftCharacterPortrait;
+		[Export] private TextureRect rightCharacterPortrait;
+		[Export] private Color enabledColor;
+		[Export] private Color disabledColor;
+		[Export] private int charactersPerSecond = 30;
 
-        [Export] private string textToDisplay = "";
-		private DialogueState state;
+		[Export] private DialogueTree dialogue;
+
+		[Export] private string textToDisplay = "";
+		private DialogueState state = DialogueState.INACTIVE;
 
 		private float numOfCharsDisplayed = 0;
 
@@ -24,77 +30,114 @@ namespace DialogueSystem
 			if(instance == null)
 			{
 				instance = this;
+				if(dialogue == null)
+				{
+					ChangeState(DialogueState.INACTIVE);
+				}
+				else
+				{
+					ChangeState(DialogueState.ACTIVATING);
+				}
 			}
 		}
 
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
-			DisplayText(delta);
+			
 		}
 
 		public static void SetText(string text)
-        {
-            instance.textToDisplay = text;
-        }
+		{
+			instance.textToDisplay = text;
+		}
 
+
+		/****************
+		 * States
+		 * *****************/
 
 		private void RunState(double delta)
 		{
-            switch (state)
-            {
-                case DialogueState.INACTIVE:
-                    break;
-                case DialogueState.ACTIVATING:
-                    break;
-                case DialogueState.REVEALING_TEXT:
-                    break;
-                case DialogueState.TEXT_DISPLAYED:
-                    break;
-            }
-        }
+			switch (state)
+			{
+				case DialogueState.INACTIVE:
+					break;
+				case DialogueState.ACTIVATING:
+					break;
+				case DialogueState.REVEALING_TEXT:
+					RevealTextState(delta);
+					break;
+				case DialogueState.TEXT_DISPLAYED:
+					break;
+			}
+		}
 
 		private void ChangeState(DialogueState newState)
 		{
-            switch (state)
-            {
-                case DialogueState.INACTIVE:
-                    break;
-                case DialogueState.ACTIVATING:
-                    break;
-                case DialogueState.REVEALING_TEXT:
-                    break;
-                case DialogueState.TEXT_DISPLAYED:
-                    break;
-            }
+			switch (state)
+			{
+				case DialogueState.INACTIVE:
+					break;
+				case DialogueState.ACTIVATING:
+					break;
+				case DialogueState.REVEALING_TEXT:
+					break;
+				case DialogueState.TEXT_DISPLAYED:
+					break;
+			}
 
-            switch (newState)
-            {
-                case DialogueState.INACTIVE:
-                    break;
-                case DialogueState.ACTIVATING:
-                    break;
-                case DialogueState.REVEALING_TEXT:
-                    break;
-                case DialogueState.TEXT_DISPLAYED:
-                    break;
-            }
-        }
+			switch (newState)
+			{
+				case DialogueState.INACTIVE:
+					dialogueBox.Visible = false;
+					break;
+				case DialogueState.ACTIVATING:
+					dialogueBox.Visible = true;
+					break;
+				case DialogueState.REVEALING_TEXT:
+					break;
+				case DialogueState.TEXT_DISPLAYED:
+					numOfCharsDisplayed = 0;
+					break;
+			}
+		}
 
-        
+		private void RevealTextState(double delta)
+		{
+			if (Input.IsActionJustPressed("ui_accept"))
+			{
+				textLabel.Text = textToDisplay;
+				ChangeState(DialogueState.TEXT_DISPLAYED);
+			}
+
+			numOfCharsDisplayed += (float)delta * charactersPerSecond;
+			string substring = textToDisplay.Substr(0, (int)numOfCharsDisplayed);
+			textLabel.Text = substring;
+
+			if (substring == textToDisplay)
+			{
+				ChangeState(DialogueState.TEXT_DISPLAYED);
+			}
+		}
+
+		private void TextDisplayedState(double delta)
+		{
+			if (Input.IsActionJustPressed("ui_accept"))
+			{
+				ChangeState(DialogueState.REVEALING_TEXT);
+			}
+		}
 
 
+		/**************
+		 * Helper
+		 * ***************/
 
-        private void SetName(string name)
+		private void SetName(string name)
 		{
 			if(nameLabel != null)
 				nameLabel.Text = name;
 		}
-
-		private void DisplayText(double delta)
-		{
-            numOfCharsDisplayed += (float)delta * charactersPerSecond;
-            textLabel.Text = textToDisplay.Substr(0, (int)numOfCharsDisplayed);
-        }
-    }
+	}
 }
